af2574d5bece6f1e3852f0ea0e7e29420dcd742c
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-11-05 20:13:35 -0300

    task list hecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './button/button.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
