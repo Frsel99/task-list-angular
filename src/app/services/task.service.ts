619cec78f5419c24ba273a85433dfedf7e414e6f
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-11-06 02:03:50 -0300

    Add Task, Remove Task y Change Reminder

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ConnectableObservable, Observable, of } from 'rxjs';
 import { Task } from '../Tasks';
-import { HttpClient, HttpHandler } from '@angular/common/http'
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http'
+
+const httpOption = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+}
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class TaskService {
     private apiUrl = "http://localhost:5000/tasks";
 
@@ -13,5 +20,19 @@ export class TaskService {
 
     getTasks(): Observable<Task[]> {
         return this.http.get<Task[]>(this.apiUrl);
+    }
+
+    deleteTask(task: Task): Observable<Task> {
+        const url = `${this.apiUrl}/${task.id}`;
+        return this.http.delete<Task>(url);
+    }
+
+    toggleReminder(task: Task): Observable<Task> {
+        const url = `${this.apiUrl}/${task.id}`;
+        return this.http.put<Task>(url, task, httpOption);
+    }
+
+    addTask(task: Task): Observable<Task> {
+        return this.http.post<Task>(this.apiUrl, task, httpOption);
     }
 }
